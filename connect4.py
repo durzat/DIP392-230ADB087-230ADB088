ce7b494a728e926e09e0d8241cd778aa5e2a8c00
Valentin Cudey <valentin.cudey@epita.fr>
2024-05-28 11:22:23 +0200

    add comment

--- connect4.py
+++ connect4.py
@@ -1,9 +1,11 @@
 import pygame
 import sys
 
+
+
 # Constants for the game
 WIDTH = 700
-HEIGHT = 700  # Adjusted to fit a 6 row grid properly
+HEIGHT = 700
 ROW_COUNT = 6
 COLUMN_COUNT = 7
 BLUE = (0, 0, 255)
@@ -13,33 +15,41 @@ RED = (255, 0, 0)
 SQUARESIZE = 100
 RADIUS = int(SQUARESIZE / 2 - 5)
 
-pygame.init()
-
 # Set up the display
+pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Connect Four")
-
 font = pygame.font.SysFont("monospace", 30)
+
+
 
 class BoardGame:
     def __init__(self):
         self.board = self.init_board()
 
+    # Initializes the game board by creating a 2D matrix (list of lists) filled with zeros, representing an empty board.
     def init_board(self):
         return [[0 for _ in range(COLUMN_COUNT)] for _ in range(ROW_COUNT)]
 
+    # Adds a token to the game board at the position specified by the row and column coordinates.
+    # The token is represented by a value (1 for player 1 and 2 for player 2).
     def add_token(self, row, col, piece):
         self.board[row][col] = piece
+
 
 class GameTurn:
     def __init__(self):
         self.turn = 0
-
+    
+    # Increments the current round of the game. Changes player (alternating between 0 and 1) after each round.
     def new_turn(self):
         self.turn = (self.turn + 1) % 2
         return self.turn
 
+
 class GameState:
+    # Checks if the specified player (represented by piece) has won.
+    # This includes checks for horizontal, vertical and diagonal alignments of four identical tokens.
     @staticmethod
     def check_if_win(board, piece):
         # Check horizontal locations for win
@@ -68,6 +78,7 @@ class GameState:
 
         return False
 
+    # Checks if the game ends in a tie (draw), that is, if all the cells on the board are filled without there being a winner.
     @staticmethod
     def check_if_egality(board):
         for row in board:
@@ -75,18 +86,24 @@ class GameState:
                 return False
         return True
 
+
 class Move:
+    # Checks if a movement is valid, that is to say if the chosen column is not full.
     @staticmethod
     def check_if_valid_move(board, col):
         return board[ROW_COUNT - 1][col] == 0
 
+    # Finds the next open (empty) row in a specified column where a token can be inserted.
     @staticmethod
     def get_next_open_row(board, col):
         for r in range(ROW_COUNT):
             if board[r][col] == 0:
                 return r
 
+
 class Graphic:
+    # Displays the game board on the screen.Draw the blue squares and black circles to represent the empty cells,
+    # then draw the red and green tokens according to the movements made by the players.
     @staticmethod
     def display_board(board):
         for c in range(COLUMN_COUNT):
@@ -101,16 +118,20 @@ class Graphic:
                     pygame.draw.circle(screen, GREEN, (int(c * SQUARESIZE + SQUARESIZE / 2), HEIGHT - int(r * SQUARESIZE + SQUARESIZE / 2)), RADIUS)
         pygame.display.update()
 
+    # Shows the position of the token being placed above the board based on mouse movement. 
     @staticmethod
     def display_choice(posx, turn):
         pygame.draw.rect(screen, BLACK, (0, 0, WIDTH, SQUARESIZE))
         pygame.draw.circle(screen, RED if turn == 0 else GREEN, (posx, int(SQUARESIZE / 2)), RADIUS)
         pygame.display.update()
 
+    # Displays a temporary message on the screen, for example when an invalid movement is performed.
     @staticmethod
-    def display_new_token(label):
+    def display_error(label):
         screen.blit(label, (40, 10))
         pygame.display.update()
+
+
 
 def main():
     board_game = BoardGame()
@@ -152,11 +173,12 @@ def main():
                     graphic.display_board(board_game.board)
                 else:
                     label = font.render("You can't play that >:(", 1, RED)
-                    graphic.display_new_token(label)
+                    graphic.display_error(label)
                     pygame.time.wait(1000)
 
                 if game_over:
                     pygame.time.wait(3000)
+
 
 if __name__ == "__main__":
     main()
