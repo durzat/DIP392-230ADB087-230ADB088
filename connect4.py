e64826505d218939b7245984a6867840f50ff8f1
Valentin Cudey <valentin.cudey@epita.fr>
2024-05-28 15:13:00 +0200

    add winner text on GUI

--- connect4.py
+++ connect4.py
@@ -3,19 +3,17 @@ import sys
 
 
 
-# Constants for the game
 WIDTH = 700
 HEIGHT = 700
 ROW_COUNT = 6
 COLUMN_COUNT = 7
-BLUE = (0, 0, 255)
-GREEN = (0, 255, 0)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
+BLUE = (255, 255, 255)
+GREEN = (255, 255, 0)
+BLACK = (48, 25, 52)
+RED = (255, 165, 0)
 SQUARESIZE = 100
 RADIUS = int(SQUARESIZE / 2 - 5)
 
-# Set up the display
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Connect Four")
@@ -27,12 +25,9 @@ class BoardGame:
     def __init__(self):
         self.board = self.init_board()
 
-    # Initializes the game board by creating a 2D matrix (list of lists) filled with zeros, representing an empty board.
     def init_board(self):
         return [[0 for _ in range(COLUMN_COUNT)] for _ in range(ROW_COUNT)]
 
-    # Adds a token to the game board at the position specified by the row and column coordinates.
-    # The token is represented by a value (1 for player 1 and 2 for player 2).
     def add_token(self, row, col, piece):
         self.board[row][col] = piece
 
@@ -41,36 +36,29 @@ class GameTurn:
     def __init__(self):
         self.turn = 0
     
-    # Increments the current round of the game. Changes player (alternating between 0 and 1) after each round.
     def new_turn(self):
         self.turn = (self.turn + 1) % 2
         return self.turn
 
 
 class GameState:
-    # Checks if the specified player (represented by piece) has won.
-    # This includes checks for horizontal, vertical and diagonal alignments of four identical tokens.
     @staticmethod
     def check_if_win(board, piece):
-        # Check horizontal locations for win
         for c in range(COLUMN_COUNT - 3):
             for r in range(ROW_COUNT):
                 if board[r][c] == piece and board[r][c + 1] == piece and board[r][c + 2] == piece and board[r][c + 3] == piece:
                     return True
 
-        # Check vertical locations for win
         for c in range(COLUMN_COUNT):
             for r in range(ROW_COUNT - 3):
                 if board[r][c] == piece and board[r + 1][c] == piece and board[r + 2][c] == piece and board[r + 3][c] == piece:
                     return True
 
-        # Check positively sloped diagonals
         for c in range(COLUMN_COUNT - 3):
             for r in range(ROW_COUNT - 3):
                 if board[r][c] == piece and board[r + 1][c + 1] == piece and board[r + 2][c + 2] == piece and board[r + 3][c + 3] == piece:
                     return True
 
-        # Check negatively sloped diagonals
         for c in range(COLUMN_COUNT - 3):
             for r in range(3, ROW_COUNT):
                 if board[r][c] == piece and board[r - 1][c + 1] == piece and board[r - 2][c + 2] == piece and board[r - 3][c + 3] == piece:
@@ -78,7 +66,6 @@ class GameState:
 
         return False
 
-    # Checks if the game ends in a tie (draw), that is, if all the cells on the board are filled without there being a winner.
     @staticmethod
     def check_if_egality(board):
         for row in board:
@@ -88,12 +75,10 @@ class GameState:
 
 
 class Move:
-    # Checks if a movement is valid, that is to say if the chosen column is not full.
     @staticmethod
     def check_if_valid_move(board, col):
         return board[ROW_COUNT - 1][col] == 0
 
-    # Finds the next open (empty) row in a specified column where a token can be inserted.
     @staticmethod
     def get_next_open_row(board, col):
         for r in range(ROW_COUNT):
@@ -102,8 +87,6 @@ class Move:
 
 
 class Graphic:
-    # Displays the game board on the screen.Draw the blue squares and black circles to represent the empty cells,
-    # then draw the red and green tokens according to the movements made by the players.
     @staticmethod
     def display_board(board):
         for c in range(COLUMN_COUNT):
@@ -118,14 +101,12 @@ class Graphic:
                     pygame.draw.circle(screen, GREEN, (int(c * SQUARESIZE + SQUARESIZE / 2), HEIGHT - int(r * SQUARESIZE + SQUARESIZE / 2)), RADIUS)
         pygame.display.update()
 
-    # Shows the position of the token being placed above the board based on mouse movement. 
     @staticmethod
     def display_choice(posx, turn):
         pygame.draw.rect(screen, BLACK, (0, 0, WIDTH, SQUARESIZE))
         pygame.draw.circle(screen, RED if turn == 0 else GREEN, (posx, int(SQUARESIZE / 2)), RADIUS)
         pygame.display.update()
 
-    # Displays a temporary message on the screen, for example when an invalid movement is performed.
     @staticmethod
     def display_error(label):
         screen.blit(label, (40, 10))
@@ -162,6 +143,11 @@ def main():
 
                     if GameState.check_if_win(board_game.board, 1 if game_turn.turn == 0 else 2):
                         print(f"Player {game_turn.turn + 1} wins!")
+                        if game_turn.turn + 1 == 1:
+                            label = font.render("Player 1 wins!", 1 , RED)
+                        else:
+                            label = font.render("Player 2 wins!", 1 , RED)   
+                        graphic.display_error(label)
                         game_over = True
 
                     if GameState.check_if_egality(board_game.board):
